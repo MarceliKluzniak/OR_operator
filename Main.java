15cfd4f1780d6ade7c5554beae5878db825b3470
Marceli Kluzniak <marcelikluzniak5@gmail.com>
2021-04-29 17:27:47 +0200

    Sample of OR operator

---
+++ Main.java
@@ -0,0 +1,24 @@
+package pl.mkluzniak;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // logical operators = used to connect two or more expressions
+        //                && = (AND) both conditions must be true
+        //                || = (OR) either condition must be true
+        //                ! = (NOT) reversed boolean value of condition
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("You are playing a game ! Press q or Q to quit");
+        String response = scanner.next();
+        if (response.equals("q") || response.equals("Q")) {
+            System.out.println("You quit the game!");
+        } else {
+            System.out.println("You are still playing the game *bang bang*");
+        }
+
+
+    }
+}
